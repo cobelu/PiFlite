a83e80031d86338e425c94540623c5e30dffed26
Connor Luckett <imamac96@gmail.com>
2024-03-09 20:01:30 -0800

    Using leaflet-rs yew example

---
+++ cartographer/src/README.md
@@ -0,0 +1,12 @@
+# Cartographer
+
+Build:
+
+```sh
+cargo install trunk
+# in project directory
+trunk serve
+Then open http://localhost:8080
+```
+
+`trunk build --release`
--- cartographer/src/main.rs
+++ cartographer/src/main.rs
@@ -1,16 +1,65 @@
-use warp::Filter;
+use crate::components::{
+    control::{Cities, Control},
+    map_component::{City, MapComponent, Point},
+};
+use yew::prelude::*;
+mod components;
 
-#[tokio::main]
-async fn main() {
-    // Define a warp filter for serving the HTML page
-    let html_route = warp::path!("cartographer")
-        .map(|| warp::reply::html(include_str!("resources/templates/index.html")));
+enum Msg {
+    SelectCity(City),
+}
 
-    // Combine the routes
-    let routes = html_route;
+struct Model {
+    city: City,
+    cities: Cities,
+}
 
-    // Run the warp server
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 8000))
-        .await;
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let aachen = City {
+            name: "Aachen".to_string(),
+            lat: Point(50.7597f64, 6.0967f64),
+        };
+        let stuttgart = City {
+            name: "Stuttgart".to_string(),
+            lat: Point(48.7784f64, 9.1742f64),
+        };
+        let cities: Cities = Cities {
+            list: vec![aachen, stuttgart],
+        };
+        let city = cities.list[0].clone();
+        Self { city, cities }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::SelectCity(city) => {
+                self.city = self
+                    .cities
+                    .list
+                    .iter()
+                    .find(|c| c.name == city.name)
+                    .unwrap()
+                    .clone();
+            }
+        }
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let cb = ctx.link().callback(Msg::SelectCity);
+        html! {
+            <>
+                <MapComponent city={&self.city}  />
+                <Control select_city={cb} cities={&self.cities}/>
+            </>
+        }
+    }
+}
+
+fn main() {
+    yew::Renderer::<Model>::new().render();
 }
